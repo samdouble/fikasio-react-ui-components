e7615bf26935f077f8353237f7a185390c03b725
samdouble <samleafs@protonmail.com>
2024-02-04 00:35:43 -0500

    fix: datepicker showing

---
+++ src/stories/DatePicker.stories.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import DatePicker, { DatePickerProps } from '../components/DatePicker/DatePicker';
+
+const story = {
+  title: 'DatePicker',
+  component: DatePicker,
+  argTypes: {
+    className: { control: 'text' },
+    style: { control: 'object' },
+  },
+};
+
+function Template({
+  className,
+  dateFormat,
+  defaultValue,
+  displayFormat,
+  isOpen,
+  name,
+  onChange,
+  onClose,
+  onRemoveValue,
+  shouldCloseOnSelect,
+  showRemoveValue,
+  showTimeSelect,
+  style,
+  timeCaption,
+  timeFormat,
+  timeIntervals,
+  value,
+}: DatePickerProps) {
+  return (
+    <DatePicker
+      className={className}
+      dateFormat={dateFormat}
+      defaultValue={defaultValue}
+      displayFormat={displayFormat}
+      isOpen={isOpen}
+      name={name}
+      onChange={onChange}
+      onClose={onClose}
+      onRemoveValue={onRemoveValue}
+      shouldCloseOnSelect={shouldCloseOnSelect}
+      showRemoveValue={showRemoveValue}
+      showTimeSelect={showTimeSelect}
+      style={style}
+      timeCaption={timeCaption}
+      timeFormat={timeFormat}
+      timeIntervals={timeIntervals}
+      value={value}
+    />
+  );
+}
+
+export const WithDefaultValue = Template.bind({});
+WithDefaultValue.args = {
+  defaultValue: new Date(),
+};
+
+export default story;
