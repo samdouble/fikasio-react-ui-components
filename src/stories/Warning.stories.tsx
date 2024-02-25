ab528db2ea0477a276bd13d6f780c6b75b5f3b39
samdouble <samleafs@protonmail.com>
2024-02-25 18:06:36 -0500

    v0.6.0

---
+++ src/stories/Warning.stories.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import Warning, { WarningProps } from '../components/Warning/Warning';
+
+const story = {
+  title: 'Warning',
+  component: Warning,
+  argTypes: {
+    className: { control: 'text' },
+    style: { control: 'object' },
+  },
+};
+
+function Template({
+  children,
+  className,
+  style,
+}: WarningProps) {
+  return (
+    <Warning
+      className={className}
+      style={style}
+    >
+      {children}
+    </Warning>
+  );
+}
+
+export const WarningNoStyle = Template.bind({});
+WarningNoStyle.args = {};
+
+export default story;
