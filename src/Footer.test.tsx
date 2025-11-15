6b4cbd3cc9dd12b5c36ca56805aeca5199d89028
samdouble <samleafs@protonmail.com>
2025-11-15 13:36:20 -0500

    fix: fix errors when importing components from the library

--- src/Footer.test.tsx
+++ src/Footer.test.tsx
@@ -7,4 +7,14 @@ describe('Footer', () => {
     const { baseElement } = render(<Footer />);
     expect(baseElement).toMatchSnapshot();
   });
+
+  it('Renders correctly with children', () => {
+    const { baseElement } = render(<Footer
+      childrenLeft={[<div key="1">Child 1</div>]}
+      childrenCenter={[<div key="2">Child 2</div>]}
+      childrenRight={[<div key="3">Child 3</div>]}
+      childrenTop={[<div key="4">Child 4</div>]}
+    />);
+    expect(baseElement).toMatchSnapshot();
+  });
 });
