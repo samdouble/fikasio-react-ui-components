223ff54261e0e404a621d505de610440e9b56fdd
samdouble <samleafs@protonmail.com>
2025-11-16 10:52:17 -0500

    feat(DropDown): improve visual by removing extra padding

--- src/components/DropdownOptions/DropdownOptions.tsx
+++ src/components/DropdownOptions/DropdownOptions.tsx
@@ -1,16 +1,13 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import classNames from 'classnames';
-import Dropdown from 'react-bootstrap/Dropdown';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faTimes } from '@fortawesome/free-solid-svg-icons';
-import DropdownToggle from './DropdownToggle';
+import { faTimes, faEllipsis } from '@fortawesome/free-solid-svg-icons';
 import useTheme from '../../hooks/useTheme';
 import convertClassNameToObj from '../../utils/convertClassNameToObj';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './DropdownOptions.scss';
 
-library.add(faTimes);
+library.add(faTimes, faEllipsis);
 
 export interface DropdownOptionsProps {
   className?: string;
@@ -28,45 +25,85 @@ export function DropdownOptions({
   style = {},
 }: DropdownOptionsProps) {
   const theme = useTheme();
+  const [isOpen, setIsOpen] = useState(false);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        containerRef.current &&
+        !containerRef.current.contains(event.target as Node)
+      ) {
+        setIsOpen(false);
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, []);
+
+  const handleToggle = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleOptionClick = (onClick?: () => void) => {
+    if (onClick) {
+      onClick();
+    }
+    setIsOpen(false);
+  };
 
   return (
     <div
+      ref={containerRef}
       className={classNames({
         'fikasio-dropdownOptions': true,
         'fikasio-theme-dark': theme === 'dark',
         'fikasio-theme-light': theme === 'light',
         ...convertClassNameToObj(className),
       })}
-      style={style}
+      style={{
+        position: 'relative',
+        ...style,
+      }}
     >
-      <Dropdown
-        style={{
-          position: 'static',
-        }}
-      >
-        <Dropdown.Toggle as={DropdownToggle} />
-        <Dropdown.Menu>
-          {
-            (options || [])
-              .map(option => (
-                <Dropdown.Item
-                  key={option.type}
-                  onClick={option.onClick}
-                >
-                  <FontAwesomeIcon
-                    icon="times"
-                    style={{
-                      color: 'red',
-                      marginRight: 10,
-                      width: 25,
-                    }}
-                  />
-                  {option.label}
-                </Dropdown.Item>
-              ))
+      <div
+        className="fikasio-dropdownOptions-toggle"
+        onClick={handleToggle}
+        onKeyDown={(e) => {
+          if (e.key === 'Enter' || e.key === ' ') {
+            e.preventDefault();
+            handleToggle();
           }
-        </Dropdown.Menu>
-      </Dropdown>
+        }}
+        role="button"
+        tabIndex={0}
+      >
+        <FontAwesomeIcon
+          icon="ellipsis"
+          size="1x"
+        />
+      </div>
+      {isOpen && options.length > 0 && (
+        <div ref={dropdownRef} className="fikasio-dropdownOptions-menu">
+          {options.map((option, index) => (
+            <div
+              key={`${option.type}-${index}`}
+              className="fikasio-dropdownOptions-item"
+              onClick={() => handleOptionClick(option.onClick)}
+            >
+              <FontAwesomeIcon
+                icon="times"
+                className="fikasio-dropdownOptions-icon"
+              />
+              {option.label}
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
