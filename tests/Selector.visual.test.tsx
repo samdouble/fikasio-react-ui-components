7749655a379aaac88d1f631b4e7dac045fc22e71
samdouble <samleafs@protonmail.com>
2026-09-14 22:02:26 -0400

    test(Selector): added tests

---
+++ tests/Selector.visual.test.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { test, expect } from '@playwright/experimental-ct-react';
+import { Selector } from '../src';
+
+const options = ['Option 1', 'Option 2', 'Option 3'];
+
+test.describe('Selector', () => {
+  test('Renders with options', async ({ mount }, testInfo) => {
+    testInfo.snapshotSuffix = '';
+    const component = await mount(
+      <div style={{
+        padding: '2rem',
+        position: 'absolute',
+        width: '400px',
+      }}>
+        <Selector options={options} />
+      </div>
+    );
+    await expect(component).toHaveScreenshot('selector-with-options.png');
+  });
+
+  test('Renders with the menu open', async ({ mount }, testInfo) => {
+    testInfo.snapshotSuffix = '';
+    const component = await mount(
+      <div style={{
+        height: '250px',
+        padding: '2rem',
+        position: 'absolute',
+        width: '400px',
+      }}>
+        <Selector options={options} />
+      </div>
+    );
+    await component.locator('.fikasio-actionbutton').click();
+    await expect(component.locator('.fikasio-selector-menu')).toBeVisible();
+    await expect(component).toHaveScreenshot('selector-menu-open.png');
+  });
+});
